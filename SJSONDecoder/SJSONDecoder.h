8bc9a9c746fd29c3c3578179b5ea868e2c71a04a
Vyacheslav Beltyukov <viacheslav@beltyukov.mobi>
2019-06-09 11:27:16 +0300

    Initial Commit

---
+++ SJSONDecoder/SJSONDecoder.h
@@ -0,0 +1,19 @@
+//
+//  SJSONDecoder.h
+//  SJSONDecoder
+//
+//  Created by Вячеслав Бельтюков on 09/06/2019.
+//  Copyright © 2019 Vyacheslav Beltyukov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SJSONDecoder.
+FOUNDATION_EXPORT double SJSONDecoderVersionNumber;
+
+//! Project version string for SJSONDecoder.
+FOUNDATION_EXPORT const unsigned char SJSONDecoderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SJSONDecoder/PublicHeader.h>
+
+
